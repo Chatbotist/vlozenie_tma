9ad91bdd9722e26d1990b7ab27dbe51e12139bae
Дмитрий <143331027+Chatbotist@users.noreply.github.com>
2024-07-22 03:27:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,18 +8,9 @@ if (window.Telegram && window.Telegram.WebApp) {
   // Включаем диалоговое окно подтверждения при закрытии
   Telegram.WebApp.enableClosingConfirmation();
 
-  // Функция для открытия сайта во встроенном браузере Telegram
-  function openWebsiteInTelegramBrowser(url) {
-    if (url.startsWith('https://') || url.startsWith('http://')) {
-      Telegram.WebApp.openInternalBrowser(url);
-    } else {
-      console.error('Invalid URL:', url);
-    }
-  }
-
   // Обработчик клика на кнопку "Open Website"
   document.getElementById('open-website').addEventListener('click', () => {
-    openWebsiteInTelegramBrowser('https://vk.com/market-212987249');
+    Telegram.WebApp.openInternalBrowser('https://vk.com/market-212987249');
   });
 
   // Обработчик изменения темы приложения
